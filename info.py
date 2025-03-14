e87a959d308d6c62ea97f75220cff266442b2d5f
mrroxstar <146523615+mrroxstar@users.noreply.github.com>
2025-03-14 11:59:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '-1002
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', 'https://envs.sh/ly.jpg')
+QR_CODE = environ.get('QR_CODE', 'https://i.ibb.co/hxb79dD5/d6bd7945ba5a.jpg')
 OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'manishkumar2532006@ybl')
 
 # ============================
